1159c6d192b0d656ca3747af2651adea5e75e667
khushi_sukar <128124987+Khushi-Sk@users.noreply.github.com>
2024-02-23 18:49:09 +0000

    Update app.js

--- app.js
+++ app.js
@@ -9,8 +9,8 @@ require('dotenv').config()
 
 
 const { createClient } = require('@supabase/supabase-js')
-const supabaseUrl = process.env.REACT_APP_SUPABASE_URL
-const supabaseKey = process.env.REACT_APP_ANON_KEY
+const supabaseUrl = process.env.SUPABASE_URL
+const supabaseKey = process.env.ANON_KEY
 const supabase = createClient(supabaseUrl, supabaseKey)
 
 const app = express();
